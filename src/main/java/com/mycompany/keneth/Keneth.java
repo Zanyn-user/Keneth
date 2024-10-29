212a637cfb22c04048db3d655ed9301572730ff9
Zanyn-user <zanynuser@gmail.com>
2024-10-29 09:23:24 -0500

    Cualquier cosa

--- src/main/java/com/mycompany/keneth/Keneth.java
+++ src/main/java/com/mycompany/keneth/Keneth.java
@@ -9,7 +9,7 @@ package com.mycompany.keneth;
  * @author User
  */
 public class Keneth {
-
+// Cualquir cosa//
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
